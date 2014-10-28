52e657843cced7a1b9e46d98f734fa85448dc2a0
Anuj Saxena <anujsaxenaa@gmail.com>
2014-10-28 15:39:09 -0400

    first page commit

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 Creating a simple online image browser
 
 Visit >> http://anujsaxenaa.github.io/Simple-Image-Browser
+
+
+This is a work in progress, below are some functionalities that being built:
+- Logically activating the previous and next buttons
+- Having images fit to page
+- Finding a way to link a local folder to the page from which the photos are accessed 
+(would thus remove the numeric buttons that currently exist)
--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ $("button").click(function () {
     if ($(this).attr("id") === "one") {
         $(".container").css("background-image", 'url(http://i283.photobucket.com/albums/kk301/brandi_bgb/pretty%20photography/sunset-glow-trees-beautiful-scenery.jpg)');
     }
-    if ($(this).attr("id") === "two") {
+    else if ($(this).attr("id") === "two") {
         $(".container").css("background-image", 'url(http://media-cdn.tripadvisor.com/media/photo-s/01/12/9f/fd/most-beach-scenery.jpg)');
     }
-    if ($(this).attr("id") === "three") {
+    else if ($(this).attr("id") === "three") {
         $(".container").css("background-image", 'url(http://p1.pichost.me/i/40/1638707.jpg)');
     }
-    if ($(this).attr("id") === "four") {
+    else if ($(this).attr("id") === "four") {
         $(".container").css("background-image", 'url(http://upload.wikimedia.org/wikipedia/commons/4/46/Greenland_scenery.jpg)');
     }
 });
