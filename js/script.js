67fda9066417859f0467e4b0d62706cf04de988d
Anuj Saxena <anujsaxenaa@gmail.com>
2014-10-28 16:00:57 -0400

    first page commit

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $("button").click(function () {
         $(".container").css("background-image", 'url(http://media-cdn.tripadvisor.com/media/photo-s/01/12/9f/fd/most-beach-scenery.jpg)');
     }
     else if ($(this).attr("id") === "three") {
-        $(".container").css("background-image", 'url(http://2.bp.blogspot.com/-fV8JB5dszyQ/UMIBOIMIdGI/AAAAAAAAOsk/1tKlZLfaJx8/s1600/Beautiful+Autumn+Scenery+Wallpapers+09.jpg)');
+        $(".container").css("background-image", 'url(http://imagecache2.allposters.com/images/EUR/2400-1425.jpg)');
     }
     else if ($(this).attr("id") === "four") {
         $(".container").css("background-image", 'url(http://www.incredible-overberg.co.za/wp-content/uploads/2012/04/Switzerland_Splendid-scenery_2618.jpg)');
